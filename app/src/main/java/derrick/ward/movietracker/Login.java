d54bcb74ab481ee83b60f962d5353f910eafd922
Derrick <dward330@gmail.com>
2020-12-07 10:00:21 -0800

    Added Sign up Screen

--- app/src/main/java/derrick/ward/movietracker/Login.java
+++ app/src/main/java/derrick/ward/movietracker/Login.java
@@ -1,6 +1,8 @@
 package derrick.ward.movietracker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,12 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+    }
+
+    /*
+    * Navigates to Sign Up Activity/Screen
+    */
+    public void navigateToSignUp(View view) {
+        startActivity(new Intent(this, SignUp.class));
     }
 }
---
+++ app/src/main/java/derrick/ward/movietracker/SignUp.java
@@ -0,0 +1,14 @@
+package derrick.ward.movietracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+}
