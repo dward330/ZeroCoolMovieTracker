3b297c275152e492964016324d9da0450cdfc25c
Derrick <dward330@gmail.com>
2020-12-07 09:38:16 -0800

    Initial Commit

---
+++ app/src/main/java/derrick/ward/movietracker/MainActivity.java
@@ -0,0 +1,59 @@
+package derrick.ward.movietracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser currentFirebaseUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.firebaseAuth = FirebaseAuth.getInstance(); // Initialize Firebase Authentication
+        this.currentFirebaseUser = this.firebaseAuth.getCurrentUser();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        new CountDownTimer(3000, 1000) {
+
+            @Override
+            public void onTick(long milliSecondsUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                if (currentFirebaseUser == null) {
+                    Toast.makeText(MainActivity.this, "No signed in user detected", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, Login.class));
+                    finish();
+                } else {
+                    if (currentFirebaseUser.isEmailVerified()) {
+                        Toast.makeText(MainActivity.this, "User is signed in", Toast.LENGTH_SHORT).show();
+                        /*
+                        startActivity( new Intent(MainActivity.this, MovieClass.class));
+                        finish();
+                        */
+                    } else {
+                        Toast.makeText(MainActivity.this, "Please verify your email and then proceed to login.", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainActivity.this, Login.class));
+                        finish();
+                    }
+                }
+            }
+        }.start();
+    }
+}
